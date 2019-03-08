5aa8f706b4c6cf24b39f4c15021edc3279fdb343
Joe7699 <48115874+Joe7699@users.noreply.github.com>
2019-03-08 08:08:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("550377128089354255")
+let channel =     client.channels.get("553443913122185218")
 setInterval(function() {
 channel.send(`اكس اكس جو`);
 }, 30)
